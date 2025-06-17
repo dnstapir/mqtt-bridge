83bff3bed8c10fbd6e5d1d5f284d07f0c6d67e13
Leon Fernandez <leon.fernandez@internetstiftelsen.se>
2025-06-18 00:06:34 +0200

    attempt at injected nodeman

--- inject/nodeman/nodeman.go
+++ inject/nodeman/nodeman.go
@@ -3,6 +3,8 @@ package nodeman
 import (
     "crypto/tls"
     "errors"
+    "fmt"
+	"io"
     "net/url"
 	"net/http"
 
@@ -19,14 +21,16 @@ type nodemanclient struct {
     client http.Client
 }
 
+const cNODEMAN_NODE_API_FMT = "/node/%s/public_key"
+
 func Create(conf Conf) (*nodemanclient, error) {
-    newClient := new(nodemanclient)
+    newNodeman := new(nodemanclient)
 
 	nodemanUrl, err := url.Parse(conf.NodemanApiUrl)
 	if err != nil {
 		return nil, errors.New("invalid nodeman api url")
 	}
-    newClient.url = nodemanUrl
+    newNodeman.url = nodemanUrl
 
 	tlsCfg := tls.Config{
 		MinVersion: tls.VersionTLS13,
@@ -42,11 +46,30 @@ func Create(conf Conf) (*nodemanclient, error) {
 		Transport: tr,
 	}
 
-    newClient.client = client
+    newNodeman.client = client
 
-    return newClient, nil
+    return newNodeman, nil
 }
 
 func (n* nodemanclient) GetKey(keyID string) ([]byte, error) {
-    return nil, errors.New("not implemented")
+	req, err := http.NewRequest("GET",
+		n.url.JoinPath(fmt.Sprintf(cNODEMAN_NODE_API_FMT, keyID)).String(),
+		nil)
+	if err != nil {
+        return nil, err
+	}
+	req.Header.Add("accept", "application/json")
+
+	rsp, err := n.client.Do(req)
+	if err != nil {
+        return nil, err
+	}
+	defer rsp.Body.Close()
+
+	body, err := io.ReadAll(rsp.Body)
+	if err != nil {
+        return nil, err
+	}
+
+    return body, nil
 }
